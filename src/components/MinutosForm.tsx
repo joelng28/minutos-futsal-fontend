f965dfa2b15013496c30c5b948b4eb53e035f925
Joel Navarro <joelnavarrogil@gmail.com>
2025-07-22 23:10:44 +0200

    avances pantalla partido

--- src/components/MinutosForm.tsx
+++ src/components/MinutosForm.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 
-
 interface Jugador {
   id: number;
   dorsal: number;
@@ -16,16 +15,14 @@ export default function TablaRotaciones() {
     { id: 2, dorsal: 7, nombre: "Luis Pérez", enPista: true, rotacionActual: 0, tiempoTotal: 0 },
     { id: 3, dorsal: 10, nombre: "Marcos Ruiz", enPista: false, rotacionActual: 0, tiempoTotal: 0 },
   ]);
-  const [contadorActivo, setContador] = useState<boolean>(true);
-  const [showModal, setShowModal] = useState(false);
+  const [contadorActivo, setContador] = useState(true);
   const [jugadorASustituirId, setJugadorASustituirId] = useState<number | null>(null);
-  const [jugadorEntranteId, setJugadorEntranteId] = useState<number | null>(null);
-  const [tiempo, setTiempo] = useState<number>(0);
+  const [tiempo, setTiempo] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
       if (contadorActivo) {
-        setTiempo (prev => prev + 1);
+        setTiempo(prev => prev + 1);
         setJugadores((prevJugadores) =>
           prevJugadores.map((jug) =>
             jug.enPista
@@ -43,46 +40,21 @@ export default function TablaRotaciones() {
     return () => clearInterval(interval);
   }, [contadorActivo]);
 
-  const abrirModal = (id: number) =>{
+  const abrirModal = (id: number) => {
     setJugadorASustituirId(id);
-    setJugadorEntranteId(null);
-    setShowModal(true);
-  }
+  };
 
-    const confirmarSustitucion = () => {
-    if (jugadorASustituirId !== null && jugadorEntranteId !== null) {
+  const confirmarSustitucion = (idEntrante: number) => {
+    if (jugadorASustituirId !== null) {
       setJugadores((prev) =>
         prev.map((jug) => {
-          if (jug.id === jugadorASustituirId) {
-            return { ...jug, enPista: false, rotacionActual: 0 };
-          } else if (jug.id === jugadorEntranteId) {
-            return { ...jug, enPista: true, rotacionActual: 0 };
-          }
+          if (jug.id === jugadorASustituirId) return { ...jug, enPista: false, rotacionActual: 0 };
+          if (jug.id === idEntrante) return { ...jug, enPista: true, rotacionActual: 0 };
           return jug;
         })
       );
-      setShowModal(false);
+      setJugadorASustituirId(null);
     }
-  };
-
-  const quitarDePista = (id: number) => {
-    setJugadores((prev) =>
-      prev.map((jug) =>
-        jug.id === id
-          ? { ...jug, enPista: false, rotacionActual: 0 } // quitar y reiniciar rotacionActual
-          : jug
-      )
-    );
-  };
-
-  const añadirAPista = (id: number) => {
-    setJugadores((prev) =>
-      prev.map((jug) =>
-        jug.id === id
-          ? { ...jug, enPista: true, rotacionActual: 0 } // añadir y reiniciar rotacionActual
-          : jug
-      )
-    );
   };
 
   const formatearTiempo = (segundos: number) => {
@@ -91,93 +63,112 @@ export default function TablaRotaciones() {
     return `${min}:${sec.toString().padStart(2, "0")}`;
   };
 
+  const jugadoresEnPista = jugadores.filter(j => j.enPista);
+  const jugadoresFueraDePista = jugadores.filter(j => !j.enPista);
+
   return (
     <div className="p-4 space-y-5">
-      <div className="bg-cyan-500 p-4 rounded shadow-md">
-        <div className="flex gap-2 items-center">
-          <text className="text-xl font-bold text-white"> Parte 1 </text>
-          <text className="text-xl font-bold text-white">{formatearTiempo(tiempo)}</text>
-          <button className="size-50" onClick={() => setContador((prev) => !prev)}>{contadorActivo ? "Pausar" : "Iniciar"}</button>
-          <button className=""> Finalizar parte </button>
+      <div className="bg-cyan-500 p-4 rounded shadow-md flex gap-4 items-center">
+        <span className="text-xl font-bold text-white">Parte 1</span>
+        <span className="text-xl font-bold text-white">{formatearTiempo(tiempo)}</span>
+        <button onClick={() => setContador(prev => !prev)}>
+          {contadorActivo ? "Pausar" : "Iniciar"}
+        </button>
+        <button>Finalizar parte</button>
+      </div>
+
+      <div className="flex gap-4">
+        {/* Tabla izquierda: Jugadores en pista */}
+        <div className="gap-2 items-center flex flex-col">
+          <div className="w-full bg-green-500 py-2 rounded text-center">
+            <span className="text-center">En pista</span>
+          </div>
+          <table className="w-full border border-collapse">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="border px-4 py-2">Dorsal</th>
+                <th className="border px-4 py-2">Nombre</th>
+                <th className="border px-4 py-2">Rotación actual</th>
+              <th className="border px-4 py-2">Total</th>
+              <th className="border px-4 py-2">Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jugadoresEnPista.map((jug) => (
+              <tr key={jug.id} className="bg-gray-100 border-b border-white">
+                <td className="px-4 py-2">{jug.dorsal}</td>
+                <td className="px-4 py-2">{jug.nombre}</td>
+                <td className="px-4 py-2">{formatearTiempo(jug.rotacionActual)}</td>
+                <td className="px-4 py-2">{formatearTiempo(jug.tiempoTotal)}</td>
+                <td className="px-4 py-2">
+                  {jugadorASustituirId === null ? (
+                    <button
+                      className="bg-red-500 text-white px-2 py-1 rounded"
+                      onClick={() => abrirModal(jug.id)}
+                    >
+                      Quitar
+                    </button>
+                  ) : jugadorASustituirId === jug.id ? (
+                    <button
+                      className="bg-yellow-500 text-white px-2 py-1 rounded"
+                      onClick={() => setJugadorASustituirId(null)}
+                    >
+                      Cancelar
+                    </button>
+                  ) : (
+                    <button
+                      className="bg-red-500/30 text-white px-2 py-1 rounded"
+                      disabled
+                    >
+                      Quitar
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+
+        {/* Tabla derecha: Jugadores fuera de pista */}
+        <div className="gap-2 items-center flex flex-col">
+          <div className="w-full bg-green-500 py-2 rounded text-center">
+            <span className="text-center">Banquillo</span>
+          </div>
+        <table className="w-full border border-collapse">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border px-4 py-2">Dorsal</th>
+              <th className="border px-4 py-2">Nombre</th>
+              <th className="border px-4 py-2">Rotación actual</th>
+              <th className="border px-4 py-2">Total</th>
+              <th className="border px-4 py-2">Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {jugadoresFueraDePista.map((jug) => (
+              <tr key={jug.id} className="bg-gray-100 border-b border-white">
+                <td className="px-4 py-2">{jug.dorsal}</td>
+                <td className="px-4 py-2">{jug.nombre}</td>
+                <td className="px-4 py-2">{formatearTiempo(jug.rotacionActual)}</td>
+                <td className="px-4 py-2">{formatearTiempo(jug.tiempoTotal)}</td>
+                <td className="px-4 py-2">
+                  <button
+                    className={`${
+                      jugadorASustituirId !== null ? "bg-green-500" : "bg-green-500/40"
+                    } text-white px-2 py-1 rounded`}
+                    disabled={jugadorASustituirId === null}
+                    onClick={() => confirmarSustitucion(jug.id)}
+                  >
+                    Añadir
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         </div>
       </div>
-      <table className="w-full border border-collapse">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="border px-4 py-2">Dorsal</th>
-            <th className="border px-4 py-2">Nombre</th>
-            <th className="border px-4 py-2">Rotación actual</th>
-            <th className="border px-4 py-2">Total</th>
-            <th className="border px-4 py-2">Acción</th>
-          </tr>
-        </thead>
-        <tbody>
-          {jugadores.map((jug) => (
-            <tr key={jug.id} className="bg-gray-100 border-b border-white">
-              <td className="px-4 py-2">{jug.dorsal}</td>
-              <td className="px-4 py-2">{jug.nombre}</td>
-              <td className="px-4 py-2">{formatearTiempo(jug.rotacionActual)}</td>
-              <td className="px-4 py-2">{formatearTiempo(jug.tiempoTotal)}</td>
-              <td className="px-4 py-2">
-                {jug.enPista ? (
-                  <button
-                    className="bg-red-500 text-white px-2 py-1 rounded"
-                    onClick={() => abrirModal(jug.id)}
-                  >
-                    Quitar
-                  </button>
-                ) : (
-                  <button
-                    className="bg-red-500/40 text-white px-2 py-1 rounded"
-                    onClick={() => añadirAPista(jug.id)}
-                    disabled
-                  >
-                    Quitar
-                  </button>
-                )}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white p-6 rounded shadow-md">
-            <h2 className="text-lg font-semibold mb-4">Selecciona al jugador que entra</h2>
-
-            <select
-              className="border px-2 py-1 mb-4 w-full"
-              value={jugadorEntranteId ?? ""}
-              onChange={(e) => setJugadorEntranteId(Number(e.target.value))}
-            >
-              <option value="">-- Seleccionar jugador --</option>
-              {jugadores
-                .filter((jug) => !jug.enPista)
-                .map((jug) => (
-                  <option key={jug.id} value={jug.id}>
-                    {jug.dorsal} - {jug.nombre}
-                  </option>
-                ))}
-            </select>
-
-            <div className="flex justify-end gap-2">
-              <button
-                className="bg-gray-300 px-4 py-2 rounded"
-                onClick={() => setShowModal(false)}
-              >
-                Cancelar
-              </button>
-              <button
-                className="bg-blue-600 text-white px-4 py-2 rounded disabled:opacity-50"
-                onClick={confirmarSustitucion}
-                disabled={jugadorEntranteId === null}
-              >
-                Confirmar
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
